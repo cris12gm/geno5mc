4c455c77f95be880925e1d53bfbbbd30c06b9d61
cris12gm <cris12gm@gmail.com>
2020-01-02 20:54:45 +0100

    visualization

--- multipleQuerySNP/forms.py
+++ multipleQuerySNP/forms.py
@@ -2,4 +2,4 @@ from django import forms
 from django.core.validators import RegexValidator
 
 class MultipleQuerySNP(forms.Form):
-    SNPids = forms.CharField(label="",initial="rs2845392\nrs2845392", widget=forms.Textarea(attrs={'cols': '15'}))
+    SNPids = forms.CharField(label="",initial="rs2845392\nrs2845392", widget=forms.Textarea(attrs={'cols': '15','rows':'5'}))
