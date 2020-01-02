9831dcebf9442be7278299adc765f11d4ce9c9bf
cris12gm <cris12gm@gmail.com>
2020-01-02 21:02:46 +0100

    -

--- queryGene/forms.py
+++ queryGene/forms.py
@@ -2,5 +2,5 @@ from django import forms
 # from querySNP.models import SNPinfo
 
 class QueryGene(forms.Form):
-    GeneId = forms.CharField(label='Gene id:',max_length=15, initial='POTEE', widget=forms.TextInput(
+    GeneId = forms.CharField(label='Gene id: (eg:ACR)',max_length=15, initial='ACR', widget=forms.TextInput(
         attrs={'placeholder':'Introduce Gene Id',}))
