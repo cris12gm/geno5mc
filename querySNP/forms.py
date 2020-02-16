7b15e81d3e7f2e939946021f86089204c8fb7eff
cris12gm <cris12gm@gmail.com>
2020-02-16 21:13:59 +0100

    changesInTour

--- querySNP/forms.py
+++ querySNP/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.core.validators import RegexValidator
 
-my_validator = RegexValidator(r"rs[0-9]{1,15}", "The snpID should be in this format: rs87894568")
+my_validator = RegexValidator(r"rs[0-9]{1,15}", "The snpID should be in this format: rs727563")
 class QuerySNP(forms.Form):
-    SNPid = forms.CharField(label='SNP ID',max_length=15, validators=[my_validator], initial="rs2845392", widget=forms.TextInput(
+    SNPid = forms.CharField(label='SNP ID',max_length=15, validators=[my_validator], initial="rs727563", widget=forms.TextInput(
         attrs={'placeholder':'Introduce SNPid',}))
--- snpsAssociated/forms.py
+++ snpsAssociated/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from django.core.validators import RegexValidator
 
-my_validator = RegexValidator(r"rs[0-9]{1,15}", "The snpID should be in this format: rs87894568")
+my_validator = RegexValidator(r"rs[0-9]{1,15}", "The snpID should be in this format: rs727563")
 
 class QuerySNP(forms.Form):
-    SNPid = forms.CharField(max_length=15, validators=[my_validator], initial="rs2845392", widget=forms.TextInput(
+    SNPid = forms.CharField(max_length=15, validators=[my_validator], initial="rs727563", widget=forms.TextInput(
         attrs={'placeholder':'Introduce SNPid',}))
 
 class QueryGene(forms.Form):
