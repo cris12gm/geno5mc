ec55703a5bb9ac2ea30c90c6e6ff2ea39238373c
cris12gm <cris12gm@gmail.com>
2020-02-20 00:52:13 +0100

    fixedTour

--- querySNP/forms.py
+++ querySNP/forms.py
@@ -3,5 +3,6 @@ from django.core.validators import RegexValidator
 
 my_validator = RegexValidator(r"rs[0-9]{1,15}", "The snpID should be in this format: rs727563")
 class QuerySNP(forms.Form):
-    SNPid = forms.CharField(label='SNP ID',max_length=15, validators=[my_validator], widget=forms.TextInput(
-        attrs={'placeholder':'rs727563',}))
+    SNPid = forms.CharField(label='SNP ID',max_length=15, validators=[my_validator], widget=forms.TextInput())
+
+    #attrs={'placeholder':'rs727563',} -> inside TextInput
